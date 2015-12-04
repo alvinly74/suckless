e9a5da2db4408ef50573acca7a840fb50cf50fb7
alvinly <alvinly74@gmail.com>
2015-12-04 12:34:20 -0800

    barebones

---
+++ app/assets/javascripts/components/Home.jsx
@@ -0,0 +1,19 @@
+var Home = React.createClass({
+  getInitialState: function(){
+    return {};
+  },
+  componentDidMount: function(){
+
+  },
+  _onChange: function(){
+
+  },
+  _className: function(){
+
+  },
+  render: function(){
+    return <div>YEABOI</div>;
+
+
+  }
+});
