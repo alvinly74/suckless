13ed2d1f0be0091aa3b5a9b51ff468e5a5486d12
alvinly <alvinly74@gmail.com>
2015-12-04 14:43:56 -0800

    update to receive last 10 played games

--- app/assets/javascripts/util/api_util.js
+++ app/assets/javascripts/util/api_util.js
@@ -1,3 +1,4 @@
+var testUserId = '510537';
 ApiUtil = {
   searchUser: function(username){
     $.ajax({
@@ -5,16 +6,18 @@ ApiUtil = {
       data: {api_key: '6482fb35-7b68-4792-8603-6aa61b8d2076'},
       method: "get",
       success: function(user){
+        console.log(user);
         ApiUtil.handleUser(user[username].id);
       }
 
     });
   },
   handleUser: function(userId){
+    var beginTime = 1415523472945;
+    var endTime = new Date().getTime().toString();
     $.ajax({
-      url: 'https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/' + userId,
-      data: {api_key: '6482fb35-7b68-4792-8603-6aa61b8d2076',
-             beginTime: new Date().getTime()- 216000},
+      url: 'https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/' + userId + "/recent",
+      data: {api_key: '6482fb35-7b68-4792-8603-6aa61b8d2076'},
       method: 'get',
       success:function(games){
         console.log(games);
