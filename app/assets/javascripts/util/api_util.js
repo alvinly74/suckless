e7ccac598d7ca010b09630d256aabd51a8fa6a91
alvinly <alvinly74@gmail.com>
2015-12-05 18:00:02 -0800

    handle user in-game and not in-game

--- app/assets/javascripts/util/api_util.js
+++ app/assets/javascripts/util/api_util.js
@@ -1,38 +1,23 @@
 var testUserId = '510537';
 ApiUtil = {
-  searchUserOriginal: function(username){
-    $.ajax({
-      url: "https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + username,
-      data: {api_key: '6482fb35-7b68-4792-8603-6aa61b8d2076'},
-      method: "get",
-      success: function(user){
-        ApiUtil.handleUser(user[username].id);
-      }
-
-    });
-  },
   searchUser: function(username){
     $.ajax({
       url: "api/search/username",
       data: {username: username},
       method: "get",
-      success: function(user){
-        debugger;
-        ApiUtil.handleUser(user[username].id);
+      success: function(currentGame){
+        console.log("Found Game");
+        ApiUtil.handleCurrentGame(currentGame);
+      },
+      error: function(testError){
+        console.log('Did not Find Game/user');
+        debugger
       }
-
     });
   },
-
-  handleUser: function(userId){
-    $.ajax({
-      url: 'https://na.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/NA1/' + userId,
-      data: {api_key: '6482fb35-7b68-4792-8603-6aa61b8d2076'},
-      method: 'get',
-      success:function(currentGame){
-        debugger;
-        console.log(currentGames);
-      }
-    });
+  handleCurrentGame: function(currentGame){
+    if (currentGame == 'problem'){
+      alert("user not in a game");
+    }
   }
 };
--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -4,11 +4,13 @@ class Api::SearchController < ApplicationController
     @username = params[:username]
     result = Net::HTTP.get(URI.parse("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + @username + "?api_key=6482fb35-7b68-4792-8603-6aa61b8d2076"))
     user_id = JSON.parse(result.gsub('=>', ':'))[@username]["id"]
-    debugger;
-    render json: user_id
+    spectator_game = Net::HTTP.get(URI.parse("https://na.api.pvp.net/observer-mode/rest/consumer/getSpectatorGameInfo/NA1/" + user_id.to_s + "?api_key=6482fb35-7b68-4792-8603-6aa61b8d2076"))
+    if spectator_game[0] == "{"
+      game = JSON.parse(spectator_game.gsub('=>', ':'))
+      render json: game
+    else
+      render json: "error"
+    end
   end
 
-  def search_params
-
-  end
 end
